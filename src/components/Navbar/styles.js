9bf06e76bf67b7c79d6de5532ec4085051473186
danilolima17 <danilodavite20@hotmail.com>
2023-09-07 11:01:03 -0300

    feat: criação do projeto e adicionando styled-components

---
+++ src/components/Navbar/styles.js
@@ -0,0 +1,12 @@
+import { styled } from "styled-components";
+
+export const Nav = styled.nav`
+    background-color: red;
+    display: flex;
+    padding: 1rem;
+    font-weight: bold;
+
+    span {
+        font-size: 2rem;
+    }
+`
